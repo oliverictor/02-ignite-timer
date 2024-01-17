76dfb93f0a8ed57fa7e5d1a66c983cf5cce9f0b9
victor.oliveira <victor.oliveira@hexagon.pro>
2024-01-17 20:34:37 -0300

    cria home page e desenvolve estilização

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,7 +14,8 @@ box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
 
 body {
     background-color: ${(props) => props.theme['gray-900']};
-    color: ${(props) => props.theme['gray-300']}
+    color: ${(props) => props.theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
 }
 
 body, input, textarea, button{
